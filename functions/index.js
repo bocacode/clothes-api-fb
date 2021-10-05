c4a167f58b0c23c0b88a30ab7da38ad59a5a0acf
Todd Albert <todd@bocacode.com>
2021-10-05 13:38:11 -0400

    added create product endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,13 @@
 const functions = require("firebase-functions")
 const express = require('express')
-const { getAllProducts, getProductById } = require('./src/products')
+const cors = require('cors')
+const { getAllProducts, getProductById, createProduct } = require('./src/products')
 
 const app = express()
+app.use(cors())
 
 app.get('/products/:productId', getProductById)
 app.get('/products', getAllProducts)
+app.post('/products', createProduct)
 
 exports.app = functions.https.onRequest(app)
--- functions/src/products.js
+++ functions/src/products.js
@@ -1,6 +1,5 @@
 const { connectDb } = require('./db')
 
-// getAllProducts
 exports.getAllProducts = (req, res) => {
   const db = connectDb()
   db.collection('clothes').get()
@@ -15,24 +14,41 @@ exports.getAllProducts = (req, res) => {
     .catch(err => res.status(500).send(err))
 }
 
-// getProductById
 exports.getProductById = (req, res) => {
-  // connect to database
   const db = connectDb()
-  // get productId from req.params
   const { productId } = req.params
-  // get document from collection
   db.collection('clothes').doc(productId).get()
     .then(doc => {
       let item = doc.data()
       item.id = doc.id
-      // return document
       res.send(item)
     })
     .catch(err => res.status(500).send(err))
 }
 
-// createProduct
+exports.createProduct = (req, res) => {
+  // check that all required fields are present
+  if(!req.body.sku || !req.body.type || !req.body.price) {
+    res.status(401).send({ message: 'Invalid request' })
+    return
+  }
+  // construct new clothing from the body
+  let newItem = {
+    sku: req.body.sku,
+    type: req.body.type,
+    price: Number(req.body.price.toFixed(2)),
+    graphic: (req.body.graphic) ? true : false,
+  }
+  if(req.body.sizes) newItem.sizes = req.body.sizes
+  if(req.body.brand) newItem.brand = req.body.brand
+  if(req.body.color) newItem.color = req.body.color
+  if(req.body.style) newItem.style = req.body.style
+
+  const db = connectDb()
+  db.collection('clothes').add(newItem)
+    .then(docRef => res.status(201).send({ id: docRef.id }))
+    .catch(err => res.status(500).send(err))
+}
 
 // updateProduct
 
