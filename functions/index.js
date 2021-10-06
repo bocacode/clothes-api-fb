48597b34f4ff1a92a77d20af5cea4fa27326b37b
Diana Tineo <diana.tineo1@gmail.com>
2021-10-06 11:42:04 -0400

    moved to cart js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions")
 const express = require('express')
 const cors = require('cors')
 const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct, deleteCartItem } = require('./src/products')
-const { deleteCartItem } = require("./src/orders")
+const { deleteCartItem } = require("./src/cart")
 
 const app = express()
 app.use(cors())
@@ -14,5 +14,5 @@ app.patch('/products/:productId', updateProduct)
 app.delete('/products/:productId', deleteProduct)
 
 
-app.delete('/orders/:itemId', deleteCartItem)
+app.delete('/cart/:itemId', deleteCartItem)
 exports.app = functions.https.onRequest(app)
---
+++ functions/src/cart.js
@@ -0,0 +1,11 @@
+const {connectDb} = require('./db')
+
+exports.deleteCartItem = (req, res) => {
+  const { itemId } = req.params;
+  const db = connectDb();
+  db.collection('orders')
+    .doc(itemId)
+    .delete()
+    .then(() => res.status(202).send({ message: "deleted" }))
+    .catch((err) => res.status(500).send(err));
+};
