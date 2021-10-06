0a12cd80540bb076d0e3d7eac97229c0f0d4dc51
Diana Tineo <diana.tineo1@gmail.com>
2021-10-06 11:39:37 -0400

    added delete cart item function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require("firebase-functions")
 const express = require('express')
 const cors = require('cors')
-const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('./src/products')
+const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct, deleteCartItem } = require('./src/products')
+const { deleteCartItem } = require("./src/orders")
 
 const app = express()
 app.use(cors())
@@ -12,4 +13,6 @@ app.post('/products', createProduct)
 app.patch('/products/:productId', updateProduct)
 app.delete('/products/:productId', deleteProduct)
 
+
+app.delete('/orders/:itemId', deleteCartItem)
 exports.app = functions.https.onRequest(app)
